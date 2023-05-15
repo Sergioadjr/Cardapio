e5448ab47e4e894440b673da24c8b23a4b4a8083
Sérgio Augusto David Júnior <sergio.adjr@gmail.com>
2023-05-15 10:58:01 -0400

    finalizando

--- cardapio-front/src/App.tsx
+++ cardapio-front/src/App.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import './App.css'
 import { Card } from "./components/card/card";
+import { CreateModal } from './components/create-modal/create-modal';
 import { useFoodData } from './hooks/useFoodData';
 import { FoodData } from "./interface/FoodData";
 
 
 function App() {
 
-  const {data} = useFoodData();
-
-  return (
+  const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpenModal = () => {
+    setIsModalOpen(prev=>!prev)
+  } 
+  
+  
+  
+  (
     <div className="container">
 
       <h1> Cardápio</h1>
@@ -21,6 +29,8 @@ function App() {
           />
         )}
       </div>
+        {isModalOpen && <CreateModal closeModal={handleOpenModal} />}
+        <button onClick={handleOpenModal}>Novo</button>
     </div>
 
   )
--- cardapio-front/src/components/create-modal/create-modal.tsx
+++ cardapio-front/src/components/create-modal/create-modal.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useFoodDataMutate } from "../../hooks/useFoodDataMutate";
+import { FoodData } from "../../interface/FoodData";
+import "./modal.css";
 
-interface CreateModalProps {
+interface InputProps {
     label: string,
     value: string,
-    updateValue(value: string | number): void
+    updateValue(value: any): void
+}
+
+interface ModalProps {
+    closeModal(): void
 }
 
 const Input = ({ label, value, updateValue }: InputProps) => {
@@ -16,21 +23,41 @@ const Input = ({ label, value, updateValue }: InputProps) => {
 }
 
 
-export function CreateModal() {
+export function CreateModal({closeModal}: ModalProps) {
     const [titulo, setTitulo] = useState("");
     const [preco, setPreco] = useState(0);
     const [imagem, setImagem] = useState("");
-    
+    const {mutate, isSuccess, isLoading} = useFoodDataMutate();  
+
+    const submit = () => {
+        const foodData: FoodData = {
+            titulo,
+            preco,
+            imagem
+        }
+        mutate (foodData)
+    }
+
+
+    useEffect(() => {
+        if (!isSuccess) return
+        closeModal();
+    }, [isSuccess])
+
+
     return (
 
         <div className="modal-overflow">
             <div className="modal-body">
                 <h2>Cadastre um novo item no cardápio</h2>
                 <form className="input-container">
-                    <Input label="titulo" value={ } updateValue />
-                    <Input label="preco" value={ } updateValue />
-                    <Input label="imagem" value={ } updateValue />
+                    <Input label="titulo" value={titulo} updateValue={setTitulo}  />
+                    <Input label="preco" value={preco.toString()} updateValue={setPreco}/>
+                    <Input label="imagem" value={imagem} updateValue={setImagem} />
                 </form>
+                <button onClick={submit} className="btn">Postar
+                {isLoading ? 'Postando...' : 'Postar'}
+                </button>
             </div>
         </div>
     )
